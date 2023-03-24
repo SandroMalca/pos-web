fcf5603f9d694f813a39eee9cb01cf4685e8b1e0
Sandro <sandro_malca@usmp.pe>
2023-03-24 18:58:10 -0500

    mas dependecias

--- pos-web/src/App.jsx
+++ pos-web/src/App.jsx
@@ -1,14 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import data from "./data";
 
 function App() {
   
 
   return (
-    <div className="App">
-     
+    <div>
+      <div className="container">
+        <div className='row'></div>
+          <div className="col-md-8">
+            {/* seccion de productos */}
+            <h1 className='text-center mt-4 mb-5'>lista de productos</h1>
+              <div className='row row-cols-1 row-cols-md-3 g-3'>
+                {
+                  data.products.map(product=>(
+                    <div key={product.id}>
+                      <img src={product.image} height={250} width="100%" />
+                      <h3>{product.name}</h3>
+                      <h5>${product.precio}</h5>
+                      <button className='btn btn-primary'>AGREGAR</button>
+                    </div>
+                  ))
+                }
+              </div>
+          </div>
+          <div className="col-md-4">
+            {/* seccion de orden de compra */}
+          </div>
+      </div>
+      <footer className='bg-dark p-3 mt-5'>
+        <p className='text-center text-white'>TENGO todos los derechos reservados, CRACK</p>
+      </footer>
     </div>
   )
 }
---
+++ pos-web/src/data.js
@@ -0,0 +1,66 @@
+const data={
+    products:[
+        {
+            id:1,
+            name:"Botella de leche",
+            precio:20,
+            image:"https://img.freepik.com/fotos-premium/botella-vidrio-leche-aislado-superficie-blanca_143106-313.jpg"
+        },
+        {
+            id:2,
+            name:"Bolsa de pan",
+            precio:5,
+            image:"https://w7.pngwing.com/pngs/288/622/png-transparent-pack-of-bread-paper-baguette-breadstick-bakery-a-bag-of-bread-baked-goods-food-accessories.png"
+        },
+        {
+            id:3,
+            name:"Plancha de huevos",
+            precio:4,
+            image:"https://w7.pngwing.com/pngs/355/747/png-transparent-egg-carton-crate-food-egg-grocery-store-bread-rice.png"
+        },
+        {
+            id:4,
+            name:"Bolsa de Arroz",
+            precio:20,
+            image:"https://d20f60vzbd93dl.cloudfront.net/uploads/tienda_011086/tienda_011086_c53fd291daba7bc769d7d3932c40bea7408310e8_producto_large_90.png"
+        },
+        {
+            id:5,
+            name:"Bolsa de papas",
+            precio:20,
+            image:"https://e7.pngegg.com/pngimages/999/446/png-clipart-potato-gunny-sack-patata-novella-izambane-vegetable-potato-food-gunny-sack.png"
+        },
+        {
+            id:6,
+            name:"Bolsa de tomates",
+            precio:20,
+            image:"https://thumbs.dreamstime.com/b/tomates-congelados-en-bolsas-de-pl%C3%A1stico-aislados-conservaci%C3%B3n-vegetal-bolsa-blanco-167460350.jpg"
+        },
+        {
+            id:7,
+            name:"Bolsa de cebollas",
+            precio:20,
+            image:"https://nectarfruit.es/wp-content/uploads/2020/05/13.-Cebolla-Bolsa-1-kg-1.jpg"
+        },
+        {
+            id:8,
+            name:"Jabon liquido",
+            precio:20,
+            image:"https://www.protex-soap.com/content/dam/cp-sites/personal-care/protex-relaunch/andina/products/01-protex-avena-para-manos.png"
+        },
+        {
+            id:9,
+            name:"Pasta dental",
+            precio:20,
+            image:"https://novafarmawimer.pe/wp-content/uploads/2022/01/Oral-B-Pasta-Dental-50-ML.jpg"
+        },
+        {
+            id:10,
+            name:"Coca Cola",
+            precio:20,
+            image:"https://doncaserito.pe/wp-content/uploads/2022/03/coca-cola-3-litros-1.jpg"
+        }       
+
+    ]
+}
+export default data;
